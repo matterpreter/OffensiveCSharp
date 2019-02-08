459f96d8807f4b288cf90f50c2230a55f5512368
matterpreter <matt@matterpreter.com>
2019-02-08 08:44:32 -0500

    Initial commit of SearchSessions

---
+++ SearchSessions/SearchSessions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace SearchSessions
+{
+    class Program
+    {
+        static void Scan(string path)
+        {
+            try
+            {
+                foreach (var file in Directory.EnumerateFiles(path, "*.exe"))
+                {
+                    Console.WriteLine("FILE: " + file);
+                }
+                foreach (var dir in Directory.EnumerateDirectories(path))
+                {
+                    Console.WriteLine("DIRECTORY: " + dir);
+                    Scan(dir);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Error: " + path);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Searching for .TXT files...");
+            Scan("C:\\");
+            Console.ReadKey();
+
+        }
+    }
+}
