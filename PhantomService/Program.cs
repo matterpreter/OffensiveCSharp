089c1db4909ab365b45fb69e45abb1adcac2861e
matterpreter <matt@matterpreter.com>
2020-01-21 13:13:30 -0500

    Adding unicode/non-printable service tool
    We can create services using Unicode and/or non-printable characters that function as intended but don't show in Task Manager, services.msc, sc.exe, etc, making them difficult for defenders to spot and remove. This tool allows us to check for and remove them. This is only the defensive portion. The offensive POC will be added shortly.

---
+++ PhantomService/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Text;
+using System.Configuration.Install;
+using System.ComponentModel;
+
+namespace PhantomService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string usage = "PhantomService.exe (audit|remove)";
+            if (args.Length == 1 && args[0].ToLower() == "audit")
+            {
+                RemovePhantomServices(false);
+            }
+            else if (args.Length == 1 && args[0].ToLower() == "remove")
+            {
+                RemovePhantomServices(true);
+            }
+            else
+            {
+                Console.WriteLine(usage);
+            }
+
+        }
+
+        static void RemovePhantomServices(bool remove)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            ServiceController[] services = ServiceController.GetServices();
+
+            foreach (ServiceController service in services)
+            {
+                string serviceName = service.ServiceName;
+
+                if (Encoding.UTF8.GetByteCount(serviceName) != serviceName.Length)
+                {
+                    Console.WriteLine("[*] Found non-ASCII service: " + service.ServiceName);
+                    if (remove)
+                    {
+                        try
+                        {
+                            ServiceInstaller ServiceInstallerObj = new ServiceInstaller();
+                            InstallContext Context = new InstallContext(null, null);
+                            ServiceInstallerObj.Context = Context;
+                            ServiceInstallerObj.ServiceName = service.ServiceName;
+                            ServiceInstallerObj.Uninstall(null);
+                            Console.WriteLine();
+                        }
+                        catch (Win32Exception w)
+                        {
+                            Console.WriteLine("[-] Failed to remove {0} -> {1}", service.ServiceName, w.Message);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
